7a4d289277c31dfbb20892d83efa120c162edee9
Cezar Sa Espinola <cezar.sa@corp.globo.com>
2013-12-03 11:40:56 -0200

    Fixed python packaging

--- python/setup.py
+++ python/setup.py
@@ -27,16 +27,13 @@ setup(
     packages=['liji'],
     package_dir={"liji": "liji"},
     include_package_data=True,
-    package_data={
-        '': ['*.xml'],
-    },
     install_requires=[
     ],
     ext_modules=[
         Extension('liji.ext._liji',
-                  ['liji/ext/_liji.c', '../liji.c'],
-                  include_dirs=['../'],
-                  depends=['setup.py', 'liji/ext/_liji.c', '../liji.c', '../liji.h'],
+                  ['liji/ext/_liji.c', 'lib/liji.c'],
+                  include_dirs=['lib'],
+                  depends=['setup.py', 'liji/ext/_liji.c', 'lib/liji.c', 'lib/liji.h'],
                   extra_compile_args=['-Wall', '-Wextra', '-Werror', '-Wno-unused-parameter'])
     ]
 )
